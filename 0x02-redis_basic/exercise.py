cb3e4eb9dfa4a9cb06d709806aadf5949591e037
Anasou1 <anasoudsassi@gmail.com>
2024-05-01 13:31:51 +0000

    adding bytes annotations for  exercice.py file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[int, str, str, float]) -> str:
+    def store(self, data: Union[int, str, bytes, float]) -> str:
         """generate a random key anad return it"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
